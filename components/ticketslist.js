67bb050c061da5f62f7a463393bbb1a4ed4d1512
Arthur Puyou <arthur@puyou.fr>
2021-07-25 13:09:21 +0200

    Show event name

--- components/ticketslist.js
+++ components/ticketslist.js
@@ -1,32 +1,4 @@
-import { useQuery, gql } from "@apollo/client";
-
-const GET_TICKETS = gql`
-  query GetEvents($eventAddress: String!) {
-    event(id: $eventAddress) {
-      id
-      name
-      tickets(first: 100) {
-        id
-        owner
-      }
-    }
-  }
-`;
-
-export default function TicketsList({ eventAddress }) {
-  const { loading, error, data } = useQuery(GET_TICKETS, {
-    variables: {
-      eventAddress,
-    },
-  });
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-  if (error) {
-    return <p>Error :(</p>;
-  }
-
+export default function TicketsList({ event }) {
   return (
     <table>
       <thead>
@@ -36,7 +8,7 @@ export default function TicketsList({ eventAddress }) {
         </tr>
       </thead>
       <tbody>
-        {data.event.tickets.map(({ id, owner }) => (
+        {event.tickets.map(({ id, owner }) => (
           <tr key={id}>
             <td>{id}</td>
             <td>{owner}</td>
--- pages/[event].js
+++ pages/[event].js
@@ -1,21 +1,57 @@
-import { useRouter } from "next/dist/client/router";
+import { useQuery, gql } from "@apollo/client";
+import { useRouter } from "next/router";
 
 import TicketsList from "../components/ticketslist";
 import Layout from "../components/layout";
 import styles from "../styles/Home.module.css";
 
+const GET_EVENTS = gql`
+  query GetEvents($eventAddress: String!) {
+    event(id: $eventAddress) {
+      id
+      name
+      tickets(first: 100) {
+        id
+        owner
+      }
+    }
+  }
+`;
+
 export default function Event() {
   const { query } = useRouter();
+  const { loading, error, data } = useQuery(GET_EVENTS, {
+    variables: { eventAddress: query.event },
+  });
+
+  const Result = () => {
+    if (loading) {
+      return <p>Loading...</p>;
+    }
+    if (error) {
+      return <p>Error :(</p>;
+    }
+
+    return (
+      <div>
+        <TicketsList event={data.event} />
+      </div>
+    );
+  };
 
   return (
     <Layout>
       <h1 className={styles.title}>GET NFT Tickets Subgraph Explorer</h1>
 
-      <h2>Tickets list for {query.event}</h2>
+      <h2>
+        Tickets list
+        <br />
+        {data.event.name}
+        <br />
+        {query.event}
+      </h2>
 
-      <div>
-        <TicketsList eventAddress={query.event} />
-      </div>
+      <Result />
     </Layout>
   );
 }
